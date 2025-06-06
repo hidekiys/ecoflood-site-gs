b73ee8ad567a76f206c2ed86c18620d6707978df
hidekiys <mathideki06@gmail.com>
2025-06-06 17:14:50 -0300

    add os objetivos

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 const openMenu = document.getElementById("open-menu");
 const menu = document.getElementById("menu");
 const closeMenu = document.getElementById("close-menu");
-
+const btnVerMais = document.getElementById("btn-ver-mais");
+const dropdown = document.getElementById("dropdown");
+const closeDropDown = document.getElementById("close-dropdown");
 openMenu.addEventListener("click", () => {
 	menu.classList.add("active");
 	openMenu.classList.add("fechado");
@@ -10,4 +12,11 @@ openMenu.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
 	menu.classList.remove("active");
 	openMenu.classList.remove("fechado");
+});
+
+btnVerMais.addEventListener("click", () => {
+	dropdown.classList.add("dropdown-active");
+});
+closeDropDown.addEventListener("click", () => {
+	dropdown.classList.remove("dropdown-active");
 });
